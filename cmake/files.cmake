e3cd28a28d251f52e88b408f72cadb7fe348c2cc
Liam Gallagher <liamcgallagher12@gmail.com>
2026-08-19 13:00:32 -0400

    Main Project Setup & CMAKE Setup

---
+++ cmake/files.cmake
@@ -0,0 +1,41 @@
+# Add sources to executable/library
+target_sources(${PROJECT_NAME} PRIVATE
+    "${CMAKE_CURRENT_SOURCE_DIR}/Core/Src/adc.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Core/Src/cordic.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Core/Src/dma.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Core/Src/gpio.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Core/Src/i2c.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Core/Src/main.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Core/Src/sai.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Core/Src/stm32g4xx_hal_msp.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Core/Src/stm32g4xx_it.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Core/Src/syscalls.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Core/Src/sysmem.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Core/Src/system_stm32g4xx.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/Core/Startup/startup_stm32g431rbtx.s"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_adc.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_adc_ex.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cordic.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_i2c.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_i2c_ex.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_sai.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_sai_ex.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/STM32Cube_FW_G4_V1.6.3/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_ll_adc.c"
+)
+
+configure_file("${CMAKE_CURRENT_SOURCE_DIR}/STM32G431RBTX_FLASH.ld" "${CMAKE_CURRENT_BINARY_DIR}" COPYONLY)
+
+set_target_properties(${PROJECT_NAME} PROPERTIES LINK_DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/STM32G431RBTX_FLASH.ld")
